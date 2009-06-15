7668324172b768a91777a111fbbd785c96f01b4d
luana.rinaldi <luana.rinaldi@c989b1ac-52df-11de-971b-8d1939f35316>
2009-06-15 19:29:29 +0000

    Performance Logger

---
+++ trunk/RNASystem/src/it/uniroma3/rnasystem/logging/ContingencyMatrixLogger.java
@@ -0,0 +1,75 @@
+package it.uniroma3.rnasystem.logging;
+
+import it.uniroma3.rnasystem.constant.Constant;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+
+public class ContingencyMatrixLogger {
+	private static Logger log = Logger.getLogger(ContingencyMatrixLogger.class); 
+	private static FileOutputStream fos ;
+	
+	public static void openFile(){
+		
+		File fileToSave = new File(Constant.CM_PATH);
+		try {
+			fos = new FileOutputStream(fileToSave);
+			
+		} catch (FileNotFoundException e) {
+			log.error ("Unable to create file ");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	private static String getHeader(){
+		String header ="\\documentclass[12pt]{amsart} \n" +
+				"\\usepackage{geometry} \n" +
+				"\\geometry{a4paper} \n" +
+				"\\begin{document}\n"+
+				"\\centering \n"; 
+		return header; 
+	}
+	private static String getTableHeader(String pattern){
+		String tableHeader = "\\begin{table}[h]\n" +
+				"\\begin{tabular}{"+pattern+"}\n"; 
+		return tableHeader; 
+	}
+	private static String getReportTableHeadLine(){
+		String header = "\\hline \n" +
+				"{\\bf{K}} & {\\bf{Class}}&{\\bf{F1-Measure}} \\\\ \n" +
+				"\\hline \\hline \n"; 
+		return header; 
+	}
+	private static String getTableHeadLine(){
+		String header = "\\hline \n" +
+				"& {\\bf{Accuracy}}  & {\\bf{Error Rate}} &{\\bf{ Precision}} & " +
+				"{\\bf{Recall}} & {\\bf{F1-Measure}} &{\\bf{ BreakEven}}  " +
+				"\\\\ \n" +
+				"\\hline \\hline \n"; 
+		return header; 
+	}
+	private static String getTableFooter(String className){
+		String tableFooter = "\\end{tabular}\n" +
+				"\\caption{Performance Measure class : "+className+"}\n" +
+				"\\end{table}\n";
+		return tableFooter; 
+	}
+	private static String getFooter(){
+		String footer = "\\end{document}"; 
+		return footer; 
+	}
+	public static void closeFile(){
+		try {
+			fos.write(getFooter().getBytes());
+			fos.close();
+		} catch (IOException e) {
+			log.error("Unable to close file"); 
+		} 
+	}
+
+}
